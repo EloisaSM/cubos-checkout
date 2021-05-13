3409545a66424350dde8f8d723eb760da64f0d57
Eloisa Sampaio <eloisa.smedina@gmail.com>
2021-05-13 20:02:36 -0300

    arquivo com funções relacionadas ao carrinho

--- carrinho.js
+++ carrinho.js
@@ -1,5 +1,11 @@
 const fs = require("fs/promises");
-const { listarProdutosEmEstoque } = require("./Produtos");
+const diasUteis = require("date-fns/addBusinessDays");
+const {
+  listarProdutosEmEstoque,
+  pegarItemEstoquePorId,
+} = require("./Produtos");
+
+const { intervalToDuration } = require("date-fns");
 
 const lerArquivoCarrinho = async () => {
   const carrinhoObj = JSON.parse(await fs.readFile("carrinho.json"));
@@ -11,35 +17,86 @@ const escreverAquivoCarrinho = async (produto) => {
   const json = fs.writeFile("carrinho.json", JSON.stringify(produto, null, 2));
 };
 
+const calcularProdutos = (produtos) => {
+  const carrinho = {
+    subtotal: 0,
+    dataEntrega: null,
+    valorDoFrete: 0,
+    totalAPagar: 0,
+  };
+
+  if (!produtos.length) {
+    return carrinho;
+  }
+
+  for (const produto of produtos) {
+    carrinho.subtotal += produto.preco * produto.quantidade;
+  }
+
+  if (carrinho.subtotal <= 20000) {
+    carrinho.valorDoFrete = 5000;
+  } else {
+    carrinho.valorDoFrete = 0;
+  }
+
+  carrinho.dataEntrega = diasUteis(new Date(), 15);
+  carrinho.totalAPagar = carrinho.subtotal + carrinho.valorDoFrete;
+
+  return carrinho;
+};
+
 const alterarCarrinho = async () => {
   const itensCarrinho = await lerArquivoCarrinho();
+  const valoresCalculados = calcularProdutos(itensCarrinho.produtos);
 
-  const produtosEmEstoque = await listarProdutosEmEstoque();
+  const carrinhoAtualizado = { ...itensCarrinho, ...valoresCalculados };
 
-  console.log(produtosEmEstoque);
+  await escreverAquivoCarrinho(carrinhoAtualizado);
 
-  const dataEntrega = +new Date();
-  const prazoFreteMili = 1296000000;
-  const dataEntregaUTC = new Date(dataEntrega + prazoFreteMili).toISOString();
+  console.log(carrinhoAtualizado);
 
-  for (const produto of produtosEmEstoque) {
-    itensCarrinho.subtotal += produto.preco * produto.quantidade;
-  }
-
-  if (itensCarrinho.subtotal <= 20000) {
-    itensCarrinho.valorDoFrete = 5000;
-  } else {
-    itensCarrinho.valorDoFrete = 0;
-  }
-
-  itensCarrinho.dataDeEntrega = dataEntregaUTC;
-  itensCarrinho.totalAPagar =
-    itensCarrinho.subtotal + itensCarrinho.valorDoFrete;
-
-  return itensCarrinho;
+  return carrinhoAtualizado;
 };
+
+async function adicionarCarrinho(id, quantidade) {
+  const produtosEmEstoque = await pegarItemEstoquePorId(id);
+
+  const produto = {
+    id,
+    quantidade,
+    nome: produtosEmEstoque.nome,
+    preco: produtosEmEstoque.preco,
+    categoria: produtosEmEstoque.categoria,
+  };
+
+  const carrinhoAtual = await lerArquivoCarrinho();
+
+  const produtoJaInserido = carrinhoAtual.produtos.find(
+    (produto) => produto.id === id
+  );
+
+  if (produtoJaInserido) {
+    carrinhoAtual.produtos.map((produto) => {
+      if (produto.id === id) {
+        return (produto.quantidade += quantidade);
+      }
+    });
+  } else {
+    carrinhoAtual.produtos.push(produto);
+  }
+
+  const calculoProdutos = calcularProdutos(carrinhoAtual.produtos);
+  const carrinhoAtualizado = { ...carrinhoAtual, ...calculoProdutos };
+
+  await escreverAquivoCarrinho(carrinhoAtualizado);
+
+  return carrinhoAtualizado;
+}
 
 module.exports = {
   lerArquivoCarrinho,
+  escreverAquivoCarrinho,
   alterarCarrinho,
+  adicionarCarrinho,
+  calcularProdutos,
 };
