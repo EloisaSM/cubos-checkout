e41cfe0863a4981c051e1f2a43a857da454cef6e
Eloisa Sampaio <eloisa.smedina@gmail.com>
2021-05-16 13:45:55 -0300

    funções para ler e escrever arquivo do estoque

--- Produtos.js
+++ Produtos.js
@@ -7,6 +7,16 @@ const lerArquivoProdutos = async () => {
   return produtosArr;
 };
 
+const escreverAquivoEstoque = async (produtos) => {
+  const json = fs.writeFile("estoque.json", JSON.stringify(produtos, null, 2));
+};
+
+const lerArquivoProdutosEstoque = async () => {
+  const produtosArr = JSON.parse(await fs.readFile("estoque.json"));
+
+  return produtosArr;
+};
+
 async function listarProdutosEmEstoque() {
   const listaProdutos = await lerArquivoProdutos();
 
@@ -14,30 +24,44 @@ async function listarProdutosEmEstoque() {
     (produto) => produto.estoque > 0
   );
 
+  await escreverAquivoEstoque(produtosEmEstoque);
+
   return produtosEmEstoque;
 }
 
 async function pegarItemEstoquePorId(id) {
-  const listaProdutos = await listarProdutosEmEstoque();
+  const listaProdutos = await lerArquivoProdutosEstoque();
 
-  const produtosEmEstoque = listaProdutos.find((produto) => produto.id === id);
+  const produtoEmEstoque = listaProdutos.find((produto) => produto.id === id);
 
-  return produtosEmEstoque;
+  return produtoEmEstoque;
 }
 
 async function estoqueSuficiente(id, quantidade) {
-  const listaProdutos = await listarProdutosEmEstoque();
+  const listaProdutos = await lerArquivoProdutosEstoque();
 
   const produtoEmEstoque = listaProdutos.find((produto) => produto.id === id);
 
   return produtoEmEstoque.estoque >= quantidade;
 }
 
+async function abaterDoEstoque(produtos) {
+  const produtosEmEstoque = await lerArquivoProdutosEstoque();
+
+  // const estoqueAbatido = produtosEmEstoque.map((produto) => {
+  //   return produto.estoque;
+  // });
+
+  // console.log(estoqueAbatido);
+}
+
 //criar funcao para abater do estoque
 
 module.exports = {
   lerArquivoProdutos,
+  lerArquivoProdutosEstoque,
   listarProdutosEmEstoque,
   pegarItemEstoquePorId,
   estoqueSuficiente,
+  abaterDoEstoque,
 };
